dbfd922d038741945711a9a49021ee9b0c00085b
wy <>
2020-05-19 18:20:11 +0800

    init

---
+++ src/web/DelEmpServlet.java
@@ -0,0 +1,35 @@
+package web;
+
+import service.EmpService;
+import service.EmpServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/** 处理delEmp?id=1 删除请求  删除员工 */
+public class DelEmpServlet extends HttpServlet {
+	public void service(HttpServletRequest request,
+                        HttpServletResponse response)
+			throws ServletException, IOException {
+		//编码
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		//1. 获取请求参数   id
+		String id=request.getParameter("id");
+		//2. 执行jdbc 删除员工数据
+		EmpService service=new EmpServiceImpl();
+		try {
+			service.removeEmp(Integer.parseInt(id));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//3. 重定向到listEmp
+		response.sendRedirect("listEmp");
+	}
+}
+
+
---
+++ web/index.jsp
@@ -0,0 +1,7 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wangyong
+  Date: 2020/5/19
+  Time: 2:46 下午
+  To change this template use File | Settings | File Templates.
+--%>
